ae244ca85dd4c2c756af0556896a5b259f56d599
Yohan Chalier <yohan@chalier.fr>
2023-03-10 10:14:21 +0100

    added confirmation before deletion

---
+++ orgapy/static/orgapy/js/misc.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    document.querySelectorAll(".link-confirm").forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            if (confirm("This can not be undone. Please confirm your decision.")) {
+                window.location.href = link.href;
+            }
+        });
+    });
+});
