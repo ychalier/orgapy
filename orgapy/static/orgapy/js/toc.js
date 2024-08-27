81268e726b26cf5c69b2d87b8536b5b84e2e0ddc
Yohan Chalier <yohan@chalier.fr>
2024-08-27 14:02:13 +0200

    enhanced toc display

--- orgapy/static/orgapy/js/toc.js
+++ orgapy/static/orgapy/js/toc.js
@@ -1,8 +1,18 @@
-function create_toc(content_container, toc_container) {
+function create_toc(content_container, toc_container) {    
+    function removeToc() {
+        toc_container.previousElementSibling.style.flexDirection = "column";
+        toc_container.parentElement.removeChild(toc_container);
+    }
     let bounds = content_container.getBoundingClientRect();
-    if (bounds.height <= 0.8 * window.innerHeight) return;
+    if (bounds.height <= 0.8 * window.innerHeight) {
+        removeToc();
+        return;
+    };
     let titles = content_container.querySelectorAll("h2");
-    if (titles.length < 2) return;
+    if (titles.length < 2) {
+        removeToc();
+        return;
+    }
     toc_container.innerHTML = "<b>Contents</b>";
     titles.forEach(title => {
         let anchor = toc_container.appendChild(document.createElement("a"));
