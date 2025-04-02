6ed902452a8b38705a34b73f41bcee23c8763204
Yohan Chalier <yohan@chalier.fr>
2025-04-02 20:43:03 +0200

    bump version to 4.4.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-orgapy',
-    version='4.4.5',
+    version='4.4.6',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GPLv3',
