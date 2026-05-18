d43e1da87874dc9bc4848d7cd1ef800ce922ae9c
Yohan Chalier <yohan@chalier.fr>
2026-05-18 10:53:38 +0200

    updated how orgapy daily summary is written

--- orgapy/management/commands/orgapy_print_daily_report.py
+++ orgapy/management/commands/orgapy_print_daily_report.py
@@ -10,19 +10,23 @@ class Command(BaseCommand):
     """Print daily report."""
     help="Print daily report with today's tasks and incoming events."
 
+    def add_arguments(self, parser):
+        parser.add_argument("user", type=str, help="Username of concerned user")
+
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
-        for user_settings in models.Settings.objects.all():
+        messages = []
+        for user_settings in models.Settings.objects.filter(user__username=kwargs["user"]):
             user = user_settings.user
-            messages = []
             for task in models.Task.objects.filter(user=user, completed=False):
                 if task.start_date == today or task.due_date == today:
-                    messages.append(f"Task: {task.title}")
+                    messages.append(f"[TSK] {task.title}")
             for calendar in models.Calendar.objects.filter(user=user):
                 for event in calendar.get_events(force=True):
                     event_start = datetime.datetime.strptime(event["dtstart"][:10], "%Y-%m-%d").date()
                     if event_start == today:
-                        messages.append(f"Event: {event['title']}")
-            if not messages:
-                continue
+                        messages.append(f"[EVT] {event['title']}")
+        if messages:
             print("\n".join(messages))
+        else:
+            print("Nothing to do.")
